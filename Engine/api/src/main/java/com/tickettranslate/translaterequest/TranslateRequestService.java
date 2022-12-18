261787051665cc540aab9c5183a8752468dc232e
TrueAquarius <98264396+TrueAquarius@users.noreply.github.com>
2022-12-18 09:53:49 +0100

    Translation round-trip completed. The real Jira ticket is now being updated.

--- Engine/api/src/main/java/com/tickettranslate/translaterequest/TranslateRequestService.java
+++ Engine/api/src/main/java/com/tickettranslate/translaterequest/TranslateRequestService.java
@@ -38,7 +38,73 @@ public class TranslateRequestService {
     }
     public boolean translate(String sourceID, String ticketID)
     {
-        String json = "empty";
+        String json = getTicket(sourceID, ticketID);
+        String description = paresTicketDescription(json);
+        String translation = translatorService.translate(description, "en", "de");
+        updateTicketDescription(sourceID, ticketID, translation);
+
+        return true;
+    }
+
+
+    private String paresTicketDescription(String ticketJson)
+    {
+        JsonParser parser = new JsonParser();
+        JsonElement rootNode = parser.parse(ticketJson);
+        JsonObject o = rootNode.getAsJsonObject();
+        String key = o.get("key").getAsString();
+        String self = o.get("self").getAsString();
+        JsonObject fields = o.get("fields").getAsJsonObject();
+        String description = fields.get("description").getAsString();
+
+        return description;
+    }
+
+
+    private void updateTicketDescription(String sourceID, String ticketID, String description)
+    {
+        String json = null;
+        //JSONObject object;
+        //description="Hello";
+        try {
+            // request url
+            String url = translationSourceURL + ticketID;
+
+            // create auth credentials
+            String base64Creds = Base64.getEncoder().encodeToString(translationSourceCredentials.getBytes());
+
+            // create headers
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Basic " + base64Creds);
+            headers.add("Content-type", "application/json");
+
+            // create body
+            //String body = "{ \"update\" : { \"components\" : [{ \"set\" : [{ \"description\" : \"" + description + "\" }]}]}}";
+            String body = "{ \"fields\" : { \"description\" : \"" + description + "\" }}";
+
+            // create request
+            HttpEntity request = new HttpEntity(body, headers);
+
+            // make a request
+            ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.PUT, request, String.class);
+
+            // get JSON response
+            json = response.getBody();
+
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return;
+    }
+
+
+
+    private String getTicket(String sourceID, String ticketID)
+    {
+        String json = null;
         //JSONObject object;
 
         try {
@@ -61,36 +127,13 @@ public class TranslateRequestService {
             // get JSON response
             json = response.getBody();
 
-            // Parse
-            JsonParser parser = new JsonParser();
-            JsonElement rootNode = parser.parse(json);
-            JsonObject o = rootNode.getAsJsonObject();
-            String key = o.get("key").getAsString();
-            String self = o.get("self").getAsString();
-            JsonObject fields = o.get("fields").getAsJsonObject();
-            String description = fields.get("description").getAsString();
-
-            String translation = translatorService.translate(description, "en", "de");
-            String dummy = json;
 
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-/*
-        RestTemplate rt = new RestTemplate();
 
-        String resp;
-
-        try {
-            resp = rt.getForObject("http://localhost:8888/rest/api/2/issue/" + ticketID, String.class);
-        }
-        catch (Exception ex)
-        {
-            resp = ex.getMessage();
-        }
-*/
-        return json==null?false:true;
+        return json;
     }
 }
 
